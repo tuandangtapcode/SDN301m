ac23c1ec09d2f5616886b0866a5f2b1ed4b91c6f
tuanpm <tuanpham081102@gmail.com>
2024-01-23 16:51:06 +0700

    update

--- client/src/pages/ANONYMOUS/Authors/index.js
+++ client/src/pages/ANONYMOUS/Authors/index.js
@@ -1,7 +1,7 @@
 import { Col, Row } from "antd";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import TableCustom from "src/components/CustomTable";
+import TableCustom from "src/components/TableCustom";
 import InputCustom from "src/components/FloatInput/InputCustom";
 import SpinCustom from "src/components/SpinCustom";
 
--- client/src/services/UserService/urls.js
+++ client/src/services/UserService/urls.js
@@ -1,3 +1,3 @@
 export const apiGetInforByGoogleLogin = "https://www.googleapis.com/oauth2/v3/userinfo"
-export const apiLogin = `${process.env.REACT_APP_ROOT_API}/user/login`
-export const apiRegister = `${process.env.REACT_APP_ROOT_API}/user/register`
+export const apiLogin = "user/login"
+export const apiRegister = "user/register"

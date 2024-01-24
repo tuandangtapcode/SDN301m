abf5bfd0e639379190d27c079379be4d7a2da013
datntse150392 <134802461+haidang0k@users.noreply.github.com>
2024-01-24 12:02:46 +0700

    Upload task with 2 func Deactive Account, View

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,31 +1,43 @@
-import UserService from '../services/user.service.js'
-
+import UserService from "../services/user.service.js";
 
 const getListAuthour = async (req, res) => {
-  const author = await UserService.fncGetListAuthor(req)
-  return res.status(author.StatusCode).json(author)
-}
+  const author = await UserService.fncGetListAuthor(req);
+  return res.status(author.StatusCode).json(author);
+};
 const getDetailProfile = async (req, res) => {
-  const detail = await UserService.fncGetDetailProfile(req)
-  return res.status(detail.StatusCode).json(detail)
-}
+  const detail = await UserService.fncGetDetailProfile(req);
+  return res.status(detail.StatusCode).json(detail);
+};
+
+// Get List Customer
+const getListCustomer = async (req, res) => {
+  const customer = await UserService.fncGetListCustomer(req);
+  return res.status(customer.StatusCode).json(customer);
+};
+
+// Deactive Account
+const deactiveAccount = async (req, res) => {
+  const deactive = await UserService.fnDeactiveAccount(req);
+  return res.status(deactive.StatusCode).json(deactive);
+};
 
 const login = async (req, res) => {
-  const respone = await UserService.fncLogin(req)
-  return res.status(respone.StatusCode).json(respone)
-}
+  const respone = await UserService.fncLogin(req);
+  return res.status(respone.StatusCode).json(respone);
+};
 
 const register = async (req, res) => {
-  const respone = await UserService.fncRegister(req)
-  return res.status(respone.StatusCode).json(respone)
-}
-
+  const respone = await UserService.fncRegister(req);
+  return res.status(respone.StatusCode).json(respone);
+};
 
 const UserController = {
   getListAuthour,
   getDetailProfile,
   login,
-  register
-}
+  register,
+  getListCustomer,
+  deactiveAccount,
+};
 
-export default UserController
+export default UserController;
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,19 +1,19 @@
-import express from 'express'
-const router = express.Router()
-import UserController from '../controllers/user.controller.js'
-import { authAdminMidleware } from '../middlewares/auth.middleware.js'
+import express from "express";
+const router = express.Router();
+import UserController from "../controllers/user.controller.js";
+import { authAdminMidleware } from "../middlewares/auth.middleware.js";
 
-router.post('/getListAuthor',
-    // authAdminMidleware,
-    UserController.getListAuthour)
-router.get('/getDetailProfile/:id',
-    UserController.getDetailProfile)
+router.post(
+  "/getListAuthor",
+  // authAdminMidleware,
+  UserController.getListAuthour
+);
 
-router.post('/login',
-    UserController.login
-)
-router.post('/register',
-    UserController.register
-)
+router.get("/getDetailProfile/:id", UserController.getDetailProfile);
 
-export default router
+router.post("/login", UserController.login);
+router.post("/register", UserController.register);
+router.get("/get-customer", UserController.getListCustomer);
+router.get("/deactive-account/:id", UserController.deactiveAccount);
+
+export default router;

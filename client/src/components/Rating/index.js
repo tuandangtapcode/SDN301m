538dafec8bfe82cea0941f724bed7bbc30d412db
badeyy <nakunl110@gmail.com>
2024-02-22 14:48:57 +0700

    update authour detail

--- client/src/components/Rating/index.js
+++ client/src/components/Rating/index.js
@@ -1,15 +1,14 @@
-import { Col, Row, Tabs } from "antd"
-import { useState } from "react"
-import { PageStyles } from "./style"
-import All from "./components/All"
-import Month from "./components/Month"
-import Week from "./components/Week"
-import SpinCustom from "../SpinCustom"
-
+import { Col, Row, Tabs } from "antd";
+import { useState } from "react";
+import { PageStyles } from "./style";
+import All from "./components/All";
+import Month from "./components/Month";
+import Week from "./components/Week";
+import SpinCustom from "../SpinCustom";
 
 const Rating = () => {
-  const [loading, setLoading] = useState(false)
-  const [activeKey, setActiveKey] = useState(1)
+  const [loading, setLoading] = useState(false);
+  const [activeKey, setActiveKey] = useState(1);
 
   const items = [
     {
@@ -27,18 +26,20 @@ const Rating = () => {
       label: `Top week`,
       children: <Week activeKey={activeKey} />,
     },
-  ]
+  ];
 
   return (
     <SpinCustom spinning={loading}>
-      <Row>
+      <Row
+      // style={{ border: "1px solid #ff5079", borderRadius: "8px" }}
+      >
         <Col span={24} className="d-flex">
           <PageStyles>
             <Tabs
               defaultActiveKey="1"
               items={items}
-              onChange={key => {
-                setActiveKey(key)
+              onChange={(key) => {
+                setActiveKey(key);
               }}
               type="card"
             />
@@ -46,7 +47,7 @@ const Rating = () => {
         </Col>
       </Row>
     </SpinCustom>
-  )
-}
+  );
+};
 
-export default Rating
+export default Rating;
